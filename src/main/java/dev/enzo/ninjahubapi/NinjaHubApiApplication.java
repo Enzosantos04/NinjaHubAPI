57670afdef67ac903e49336a7339cf8e19e67356
enzosdev <enzohfs@gmail.com>
2025-05-26 22:28:37 -0300

    First commit

---
+++ src/main/java/dev/enzo/ninjahubapi/NinjaHubApiApplication.java
@@ -0,0 +1,13 @@
+package dev.enzo.ninjahubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaHubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinjaHubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/enzo/ninjahubapi/NinjaHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enzo.ninjahubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaHubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
