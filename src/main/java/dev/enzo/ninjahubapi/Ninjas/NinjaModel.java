ecbdeb00d098b2e6138b3561de176b97dd595b50
enzosdev <enzohfs@gmail.com>
2025-05-29 16:10:15 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/enzo/ninjahubapi/Ninjas/NinjaModel.java
+++ src/main/java/dev/enzo/ninjahubapi/Ninjas/NinjaModel.java
@@ -1,11 +1,18 @@
 package dev.enzo.ninjahubapi.Ninjas;
 import dev.enzo.ninjahubapi.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity // Annotation entity que transforma a java class em um entidade do banco de dados
 @Table(name = "tb_ninjaRegister") // Annotation Tabela que vai tem as colunas de id, nome, email, idade.
 //(name = ) 'e o nome da tabela
+@NoArgsConstructor // construtor sem argumentos pela annotation do lombok
+@AllArgsConstructor //construtor com todos os argumentos pela annotation do lombok
+@Data // essa annotation cria todos os getters e setters
 public class NinjaModel {
     @Id// annotation Id preenche o id e incrementa automaticamente
     @GeneratedValue(strategy = GenerationType.IDENTITY) //
@@ -16,40 +23,8 @@ public class NinjaModel {
 
       //@ManyToOne um ninja tem uma unica missao
       @ManyToOne()
-      @JoinColumn(name = "missoes_id") // junta as colunas de missoes e de ninjas
+      // joinColumn vai criar uma coluna chamada missoes_id na tabela NinjaRegister
+      @JoinColumn(name = "missoes_id")
       private MissoesModel missoes;
 
-    public NinjaModel(int idade, String email, String nome) {
-        this.idade = idade;
-        this.email = email;
-        this.nome = nome;
-    }
-
-    public NinjaModel(){
-
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
