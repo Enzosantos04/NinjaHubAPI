3842262bbd4af0c500b0891d07e4c74506c09f7e
enzosdev <enzohfs@gmail.com>
2025-06-10 22:54:59 -0300

    fix: Rotas do MissoesController funcionando de forma correta, com todas as funcionalidades disponiveis

--- src/main/java/dev/enzo/ninjahubapi/Missoes/MissoesController.java
+++ src/main/java/dev/enzo/ninjahubapi/Missoes/MissoesController.java
@@ -3,36 +3,44 @@ package dev.enzo.ninjahubapi.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController //recebe as requisicoes HTTPP
 @RequestMapping("missoes")// mapeia as rotas das requisicoes, nesse caso o paramentro vai ser
 //para acessar /missoes primeiro para depois acessar /missoes/criar.
 public class MissoesController {
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
 
     //adicionar missao
     @PostMapping("/criar")
-    public String criarMissoes(){
-        return "criar missoes";
+    public MissoesDTO criarMissoes(@RequestBody MissoesDTO missoesDTO){
+        return missoesService.criarMissoes(missoesDTO);
+
     }
 
-    @PutMapping("/atualizar")
-    public String atualizarMissoes(){
-        return "atualizar missoes";
+    @PutMapping("/atualizarMissao/{id}")
+    public MissoesDTO atualizarMissoes(@PathVariable Long id, @RequestBody MissoesDTO missaoAtulizada){
+        return missoesService.atualizarMissaoPorId(id, missaoAtulizada);
     }
 
-    @GetMapping("/todas")
-    public String mostrarMissoes(){
-        return "todas as missoes";
+    @GetMapping("/listar")
+    public List<MissoesDTO> mostrarMissoes(){
+        return missoesService.listarMissoes();
     }
 
-    @GetMapping("/todasID")
-    public String mostrarMissoesID(){
-        return "Mostrar missoes por id";
+    @GetMapping("/listar/{id}")
+    public MissoesDTO mostrarMissoesPorId(@PathVariable Long id){
+        return missoesService.listarMissoesPorId(id);
     }
 
-    @DeleteMapping("/deletarID")
-    public String deletarID(){
-        return "deletar por ID";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarId(@PathVariable Long id){
+        missoesService.deletarMissoesPorId(id);
     }
 
 
